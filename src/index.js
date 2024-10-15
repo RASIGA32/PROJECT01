6401ae19fa02e39f718d8167be0b76fbdfa61080
RASIGA32 <rasigar94@gmail.com>
2024-10-15 10:41:59 +0530

    message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Greeting to all developers</h1>
+    <h1>hello world</h1>
   </React.StrictMode>
 );
 
